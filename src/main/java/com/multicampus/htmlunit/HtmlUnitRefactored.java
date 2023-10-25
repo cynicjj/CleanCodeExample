e4c339d0724eb9328061952ca8e59a33b657d072
cynic <cynic@DESKTOP-IR5AFJB>
2023-10-25 16:13:00 +0900

    : 2 HtmlUnit
    HtmlUnitRefactored 클래스 추가

---
+++ src/main/java/com/multicampus/htmlunit/HtmlUnitRefactored.java
@@ -0,0 +1,26 @@
+package com.multicampus.htmlunit;
+
+public class HtmlUnitRefactored {
+	public static String renderPageWithSetupsAndTeardowns(PageData pageData, boolean isSuite) throws Exception {
+		boolean isTestPage = pageData.hasAttribute("Test");
+		if (isTestPage) {
+			WikiPage testPage = pageData.getWikiPage();
+			StringBuffer newPageContent = new StringBuffer();
+			includeSetupPages(testPage, newPageContent, isSuite);
+			newPageContent.append(pageData.getContent());
+			includeTeardownPages(testPage, newPageContent, isSuite);
+			pageData.setContent(newPageContent.toString());
+		}
+		return pageData.getHtml();
+	}
+
+	private static void includeTeardownPages(WikiPage testPage, StringBuffer newPageContent, boolean isSuite) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void includeSetupPages(WikiPage testPage, StringBuffer newPageContent, boolean isSuite) {
+		// TODO Auto-generated method stub
+		
+	}
+}
